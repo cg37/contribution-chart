d240b03b35ffac2e7350250c04ff01c1cb6dc94f
Craig <cg37@outlook.com>
2025-05-31 18:46:22 +0800

    feat: 完善组件样式

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,17 @@
 <template>
-  <div class="">
-    {{ totalContributions }}
-  </div>
-  <div class="data">
-    <template v-for="(item, index) of weeksData" :key="index">
-      <WeekColumn :data="item" />
-    </template>
+  <div class="chart-container">
+    <ContributionChart :show-total-contribute="true" />
   </div>
 </template>
 <script lang="ts" setup>
-import { getContributionData } from "@/api/getContributionData";
-import { computed, onMounted, ref } from "vue";
-import WeekColumn from "./components/WeekColumn.vue";
-const contributionCalender = ref();
-const totalContributions = computed(() => {
-  return contributionCalender.value?.totalContributions;
-});
-const weeksData = computed(() => {
-  return contributionCalender.value?.weeks.map((it) => it.contributionDays);
-});
-onMounted(() => {
-  getContributionData()
-    .then(
-      (res) =>
-        (contributionCalender.value =
-          res?.user?.contributionsCollection?.contributionCalendar)
-    )
-    .catch((err) => console.log(err));
-});
+import ContributionChart from "./components/ContributionChart.vue";
 </script>
 <style lang="scss" scoped>
-.data {
-  width: 100%;
-  height: 100%;
+.chart-container {
   display: flex;
-  justify-content: center;
   align-items: center;
-  gap: 3px;
+  justify-content: center;
+  flex-direction: column;
+  gap: 12px;
 }
 </style>
---
+++ src/components/ContributionChart.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="data">
+    <template v-for="(item, index) of weeksData" :key="index">
+      <WeekColumn :data="item" />
+    </template>
+  </div>
+  <div class="total" v-show="props.showTotalContribute">
+    {{ totalContributions }}
+  </div>
+</template>
+<script lang="ts" setup>
+import { getContributionData } from "@/api/getContributionData";
+import { computed, onMounted, ref } from "vue";
+import WeekColumn from "@/components/WeekColumn.vue";
+
+const props = withDefaults(
+  defineProps<{
+    showTotalContribute?: boolean;
+    selectData?: boolean;
+  }>(),
+  {
+    showTotalContribute: false,
+    selectData: false
+  }
+);
+
+const contributionCalender = ref();
+const totalContributions = computed(() => {
+  return contributionCalender.value?.totalContributions;
+});
+const weeksData = computed(() => {
+  return contributionCalender.value?.weeks.map((it) => it.contributionDays);
+});
+onMounted(() => {
+  getContributionData()
+    .then(
+      (res) =>
+        (contributionCalender.value =
+          res?.user?.contributionsCollection?.contributionCalendar)
+    )
+    .catch((err) => console.log(err));
+});
+</script>
+<style lang="scss" scoped>
+.total {
+  border: 2px solid #444;
+  border-radius: 4px;
+  padding: 6px 24.2px;
+  font-size: 18px;
+  user-select: none;
+  &:hover {
+    background-color: #333;
+  }
+  transition: background-color 0.3s ease;
+}
+.data {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 3px;
+  overflow: hidden;
+}
+</style>
