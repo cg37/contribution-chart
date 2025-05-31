81cdb40ae42fe5f98a5217e1b0cdc31bf558cc2d
Craig <cg37@outlook.com>
2025-05-31 19:37:42 +0800

    build: 添加element-plus 组件

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,21 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import svgLoader from "vite-svg-loader";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader()],
+  plugins: [
+    vue(),
+    svgLoader(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    })
+  ],
   server: {
     host: "0.0.0.0",
     port: 3031
