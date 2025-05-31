4a02e5e17957e7fd887a4cb9195cd63a9910a937
Craig <cg37@outlook.com>
2025-05-31 12:46:31 +0800

    feat: 简易的在前端返回报文

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,26 @@
 <template>
   <RouterLink to="/home">主页</RouterLink>
   <RouterLink to="/about">关于</RouterLink>
-  <RouterView />
+  <RouterView>
+    <div class="data">
+      {{ data }}
+    </div>
+  </RouterView>
 </template>
+<script lang="ts" setup>
+import { getContributionData } from "@/api/getContributionData";
+import { onMounted, ref } from "vue";
+const data = ref();
+onMounted(() => {
+  getContributionData()
+    .then((res) => (data.value = res))
+    .catch((err) => console.log(err));
+});
+</script>
+<style lang="scss" scoped>
+.data {
+  border: 1px solid red;
+  width: 100%;
+  height: 100px;
+}
+</style>
